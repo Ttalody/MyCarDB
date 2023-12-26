4c282189f2009de89c3cf6fd03bfb8b19f7ea37a
Артур <Artur@MacBook-Air-Elvira.local>
2023-12-27 00:50:55 +0300

    Initial commit

---
+++ MyCarDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCarDB
+//
+//  Created by Артур on 27.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
